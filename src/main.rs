6dced133a126b332185feeb4ea2f58fb7059c0f0
jamie <jameiapps101@gmail.com>
2022-03-14 21:04:36 +0000

    Fixed weird printing bug after exiting game

--- src/main.rs
+++ src/main.rs
@@ -31,28 +31,38 @@ fn main() {
     let dims : [usize; 2] = args_to_dims(dim_string).unwrap();
 
     // Start the Game
-    let mut gs = GameState::new(dims,[0,0]);
-    gs.gen_food();
-    let mut ui = UI::new().unwrap();
-    ui.clear();
-    loop {
-        let control = ui.get_control();
-        if control==UIControl::ExitProgram {
-            ui.clear();
-            break;
-        }
-        // update the game state base on user input
-        // render out the game for the ui
-        let (ss,rd) = gs.update_and_render(control.get_snake_control(), true);
-        if let SnakeState::Dead(reason) = ss {
-            ui.clear();
-            println!("You died: {:?}",reason);
-            break;
-        }
-        // update ui
-        ui.render(rd);
-        // wait a while
-        sleep(Duration::from_millis(500));
+    let out: Option<String>;
+    // Use additional scope here to ensure UI element is dropped before we print
+    // the final score. This stops the terminal from being mucked up.
+    {
+        let mut gs = GameState::new(dims,[0,0]);
+        gs.gen_food();
+        let mut ui = UI::new().unwrap();
+        ui.clear();
+        loop {
+            let control = ui.get_control();
+            if control==UIControl::ExitProgram {
+                ui.clear();
+                out = None;
+                break;
+            }
+            // update the game state base on user input
+            // render out the game for the ui
+            let (ss,rd) = gs.update_and_render(control.get_snake_control(), true);
+            if let SnakeState::Dead(reason) = ss {
+                ui.clear();
+                out = Some(format!("You died by {:?}, score: {}",reason, rd.score));
+                break;
+            }
+            // update ui
+            ui.render(rd);
+            // wait a while
+            sleep(Duration::from_millis(500));
+        };
+    }
+
+    if let Some(line) = out {
+        println!("{}",line);
     }
 
 }
